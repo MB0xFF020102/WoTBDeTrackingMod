d0fe43812ed171d5585e9680d9abc5a78c864915
NoNickss <48417412+WotblitzReversing@users.noreply.github.com>
2022-06-29 13:23:56 +0200

    Replacing Tracks Textures working (+ isDVPL)

--- UnitTests/ColorsReplacer.py
+++ UnitTests/ColorsReplacer.py
@@ -13,12 +13,55 @@ def CollectFilePathList():
 
     return filePathList
 
+def ColorsReplacerSwitcher( filePath, colorEnabled ):
+
+    print("Track FilePath Found:", filePath)
+
+    isDVPL = False
+    if ".dvpl" in filePath:
+        isDVPL = True
+
+    colorBuffer = []
+
+    if colorEnabled == "CYAN":
+        colorBuffer = ColorsConst.NORMAL_CYAN
+        if isDVPL == True:
+            colorBuffer = ColorsConst.PACKED_CYAN
+
+    elif colorEnabled == "GREEN":
+        colorBuffer = ColorsConst.NORMAL_GREEN
+        if isDVPL == True:
+            colorBuffer = ColorsConst.PACKED_GREEN
+
+    elif colorEnabled == "PINK":
+        colorBuffer = ColorsConst.NORMAL_PINK
+        if isDVPL == True:
+            colorBuffer = ColorsConst.PACKED_PINK
+
+    elif colorEnabled == "RED":
+        colorBuffer = ColorsConst.NORMAL_RED
+        if isDVPL == True:
+            colorBuffer = ColorsConst.PACKED_RED
+
+    elif colorEnabled == "YELLOW":
+        colorBuffer = ColorsConst.NORMAL_CYAN
+        if isDVPL == True:
+            colorBuffer = ColorsConst.PACKED_YELLOW
+
+    print("Overwriting To Filled Color:", filePath)
+
+    colorReplacer = open(filePath, "wb")
+    colorReplacer.write(colorBuffer)
+    colorReplacer.close()
+
 def ColorsReplacer( colorEnabled ):
+
+    print("Replacing Color is:", colorEnabled)
 
     filePathList = CollectFilePathList()
 
     for filePath in filePathList:
         if "track.dx11" in filePath:
-            print(filePath)
+            ColorsReplacerSwitcher(filePath, colorEnabled)
 
-ColorsReplacer("colorEnabled")
+ColorsReplacer("CYAN")
